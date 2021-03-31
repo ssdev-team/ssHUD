78ce564165b17ff1674c946885eb4ae2e05f3f4d
Jacky <38072174+Jacky-es@users.noreply.github.com>
2021-03-31 19:29:50 -0400

    Update sv_init.lua

--- lua/autorun/server/sv_init.lua
+++ lua/autorun/server/sv_init.lua
@@ -1,3 +1,7 @@
+print("[SSHUD] Loading ...")
+
 AddCSLuaFile("autorun/cl_scoreboard.lua")
-resource.AddFile("addons/orp_scoreboard/lua/autorun/server/sv_init.lua")
-resource.AddFile("addons/orp_scoreboard/lua/autorun/cl_hud.lua")
+resource.AddFile("lua/autorun/server/sv_init.lua")
+resource.AddFile("lua/autorun/cl_hud.lua")
+
+print("[SSHUD] Finished Loading")
