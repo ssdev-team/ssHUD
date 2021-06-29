d26ccc359783714998d8435c8f16818e56f5aecd
Aiko Suzuki <42787030+Aiko-Suzuki@users.noreply.github.com>
2021-06-29 00:27:00 -0400

    Delete sv_init.lua useless file
    everything in autorun gets automatically send to the client except if it's in `autorun/server`.
    
    don't fucking use resource.AddFile for Lua files

--- lua/autorun/server/sv_init.lua
+++
@@ -1,7 +0,0 @@
-print("[SSHUD] Loading ...")
-
-AddCSLuaFile("autorun/cl_scoreboard.lua")
-resource.AddFile("lua/autorun/server/sv_init.lua")
-resource.AddFile("lua/autorun/cl_hud.lua")
-
-print("[SSHUD] Finished Loading")
